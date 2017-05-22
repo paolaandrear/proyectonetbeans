a1a6117fe3db345b6cc06ea071a11e2072b5b45b
paolaandrear <paoramos1999@gmail.com>
2017-05-22 08:16:36 -0500

    Primer commit

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author paolaramos
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Te Amo <3!");
+    }
+    
+}
